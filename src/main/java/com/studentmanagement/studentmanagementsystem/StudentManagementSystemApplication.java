dfff1749fb76c032b782a40f8f2d1120180d8143
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-11-08 21:23:00 +0530

    added students manually.

--- src/main/java/com/studentmanagement/studentmanagementsystem/StudentManagementSystemApplication.java
+++ src/main/java/com/studentmanagement/studentmanagementsystem/StudentManagementSystemApplication.java
@@ -1,13 +1,31 @@
 package com.studentmanagement.studentmanagementsystem;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.studentmanagement.studentmanagementsystem.entity.Student;
+import com.studentmanagement.studentmanagementsystem.repository.StudentRepository;
+
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Student student1 = new Student("Dhwaj", "Sharma", "dhwajsharma10@gmail.com");
+		studentRepository.save(student1);
+
+		Student student2 = new Student("Ramesh", "Sharma", "rameshsharma10@gmail.com");
+		studentRepository.save(student2);
+
 	}
 
 }
