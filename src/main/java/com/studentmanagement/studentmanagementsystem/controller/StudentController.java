26363cf45148efbfacd0da950b54f6ed978c4b0f
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-11-12 20:53:01 +0530

    Implemented Update Functionality.

--- src/main/java/com/studentmanagement/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/studentmanagement/studentmanagementsystem/controller/StudentController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.studentmanagement.studentmanagementsystem.entity.Student;
@@ -36,6 +37,29 @@ public class StudentController {
     @PostMapping("students")
     public String saveStudent(@ModelAttribute("student") Student student) {
         studentService.saveStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/edit/{id}")
+    public String editStudentForm(@PathVariable Long id, Model model) {
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "edit_students";
+    }
+
+    @PostMapping("/students/{id}")
+    public String updateStudent(@PathVariable Long id,
+            @ModelAttribute("student") Student student,
+            Model model) {
+
+        // get student from database by id
+        Student existingStudent = studentService.getStudentById(id);
+        existingStudent.setId(id);
+        existingStudent.setFirstName(student.getFirstName());
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setEmail(student.getEmail());
+
+        // save updated student object
+        studentService.updateStudent(existingStudent);
         return "redirect:/students";
     }
 
